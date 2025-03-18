cfbb15a6905fc2b8086a8cf86ca416c609b17f6e
Alpha Yu <xyz199809@gmail.com>
2025-03-18 22:35:06 +0800

    feat: 添加客户能力仪表板组件，集成iframe展示功能

---
+++ src/views/maint/eventbus/cust-cap-dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-container">
+    <iframe frameborder="0" :src="iframeSrcUrl" />
+  </div>
+</template>
+<script lang="ts" setup name="CustCapDashboard">
+import { getAccessToken } from "@/utils/auth";
+const baseUrl = import.meta.env.VITE_APP_BASE_API;
+var token = getAccessToken();
+const iframeSrcUrl =
+  baseUrl + "/cust/cap/index.html?perm=eventbus-cust-dashboard&access_token=" + token;
+</script>
+<style lang="scss" scoped>
+/** 关闭tag标签  */
+.app-container {
+  /* 50px = navbar = 50px */
+  height: calc(100vh - 50px);
+}
+
+/** 开启tag标签  */
+.hasTagsView {
+  .app-container {
+    /* 84px = navbar + tags-view = 50px + 34px */
+    height: calc(100vh - 84px);
+  }
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
